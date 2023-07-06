8f614e767cec0f70ad403defb3d0aa2680b50832
Kenta Kozuka <kenta.kozuka@gmail.com>
2023-07-06 22:45:36 +0900

    Fix deprecated attributes (#2)
    Signed-off-by: Kenta Kozuka <kenta.kozuka@gmail.com>

--- ecs/ecs.tf
+++ ecs/ecs.tf
@@ -1,14 +1,17 @@
 resource "aws_ecs_cluster" "this" {
   name = var.project
 
-  capacity_providers = [
-    "FARGATE",
-    "FARGATE_SPOT"
-  ]
-
   tags = {
     Name = "${var.project}"
   }
+}
+
+resource "aws_ecs_cluster_capacity_providers" "this" {
+  cluster_name       = aws_ecs_cluster.this.name
+  capacity_providers = [
+    "FARGATE",
+    "FARGATE_SPOT",
+  ]
 }
 
 resource "aws_ecs_task_definition" "this" {
--- vpc/vpc.tf
+++ vpc/vpc.tf
@@ -1,7 +1,5 @@
 resource "aws_vpc" "main" {
   cidr_block                       = "10.0.0.0/16"
-  enable_classiclink               = false
-  enable_classiclink_dns_support   = false
   enable_dns_hostnames             = true
   enable_dns_support               = true
   instance_tenancy                 = "default"
